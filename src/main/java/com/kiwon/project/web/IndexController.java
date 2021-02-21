b5a3513b6075b0622ee776ebf00e8699d2bedddf
kkiwo <kkiwon2>
2021-02-21 20:00:43 +0900

    final

--- src/main/java/com/kiwon/project/web/IndexController.java
+++ src/main/java/com/kiwon/project/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.kiwon.project.web;
 
+import com.kiwon.project.config.auth.LoginUser;
+import com.kiwon.project.config.auth.dto.SessionUser;
 import com.kiwon.project.services.psts.PostsService;
 import com.kiwon.project.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -16,22 +20,24 @@ public class IndexController {
     private final PostsService postsService;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getEmail());
+        }
         return "index";
     }
 
     @GetMapping("/posts/save")
-    public String postsSave(){
+    public String postsSave() {
         return "posts-save";
     }
 
-    @GetMapping("/posts/update/{idD}")
-    public String postsUpdate(@PathVariable("idD") Long id, Model model) {
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
         PostsResponseDto dto = postsService.findById(id);
         model.addAttribute("post", dto);
 
         return "posts-update";
     }
-
 }
